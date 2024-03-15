6412336d9075e7c355acb9c98237645ad066306e
Bruno Miguel Brás <miguelazevedodg@gmail.com>
2024-03-15 19:59:52 +0000

    Primeiro Template

--- setup/urls.py
+++ setup/urls.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from django.urls import path
 
 from todos.views import home
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', home),
+    path("admin/", admin.site.urls),
+    path("", home),
 ]
